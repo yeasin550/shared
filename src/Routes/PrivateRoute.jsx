c9695f3120f70f72678b36e34deb1e0021ed65d9
yeasin550 <yeasin51570@gmail.com>
2023-06-11 15:31:48 +0800

    text

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+
+
+import { useContext } from "react";
+import { Navigate } from "react-router";
+import { useLocation } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
+
+  if (user) {
+    return children;
+  }
+    if (loading) {
+      return <progress className="progress w-56 text-center"></progress>;
+    }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
